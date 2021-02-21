1d5630563254df6b9eede27e65dcf18a10105a3b
RayFisher-24 <arijitmondal0024@gmail.com>
2021-02-21 21:59:40 +0530

    add oops

--- Ch10_Inheritance.java
+++ Ch10_Inheritance.java
@@ -44,7 +44,7 @@ class Base1{
 
 class Base2 extends Base1{
 
-    @override
+    @Override
     public void meth2(){
         System.out.println("I am Meth2 of Base2");
     }
@@ -52,6 +52,69 @@ class Base2 extends Base1{
         System.out.println("I am Meth3");
     }
 
+}
+
+//(Problem-1)Create a class circle and use inheritance to create another class cylinder from it. Create method for area and volume
+class Circle{
+    public double radius;
+
+    Circle(double r){
+       // System.out.println("The radius of the Circle");
+        this.radius =r;
+    }
+    public double resultOfArea(){
+        return Math.PI*this.radius*this.radius;
+    }
+    public double volumeOfCircle(){
+        return 4/3*Math.PI*this.radius*this.radius*this.radius;
+    }
+
+}
+class Cyclinder extends Circle{
+    public double height;
+
+    Cyclinder(double r, double h){
+        super(r);
+        //System.out.println("The radius of the Circle");
+        this.height = h;
+    }
+    public double resultOfVolume(){
+        return Math.PI*this.radius*this.radius*this.height;
+    }
+}
+
+//(Problem-2)Create a class retangle and use inheritance to create another class cuboid . try to keep it as close to real world sceario as passible
+class Retangle{
+    public double width;
+    public double length;
+
+    Retangle(){
+        System.out.println("I am a Contructor of Retangle class");
+    }
+    Retangle(double width, double length){
+        System.out.println("I am a Method Loaded Contructor of Retangle calss");
+        this.width = width;
+        this.length = length;
+    }
+
+    public double area(){
+        double result = this.width*this.length;
+        return result;
+    }
+}
+
+class Cuboid extends Retangle{
+    public double height;
+
+    Cuboid(double width, double length, double height){
+        super(width, length);
+        System.out.println("I am a method overridding by Cuboid from Retangle");
+        this.height =height;
+    }
+    public double volume(){
+        double result = this.width*this.length*this.height;
+        return result;
+    }   
 }
 
 public class Ch10_Inheritance { 
@@ -71,8 +134,25 @@ public class Ch10_Inheritance {
 
         Base1 b = new Base1();
         //b.meth2();
+        //b.meth3();
 
         Base2 bb = new Base2();
         bb.meth2();
+        bb.meth3();
+
+        //Problem-1
+        Circle c = new Circle(6);
+        System.out.println(c.resultOfArea());
+        System.out.println(c.volumeOfCircle());
+
+        Cyclinder cy = new Cyclinder(7,9);
+       System.out.println(cy.resultOfVolume());
+
+        //Problem-2
+        //Retangle re = new Retangle(12, 9);
+        //System.out.println(re.area());
+
+        Cuboid cd = new Cuboid(12,9,21);
+        System.out.println(cd.volume());
     }
 }
