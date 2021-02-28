3b5d0421f38aab20887bbc251fe368ad0ca9a25f
RayFisher-24 <arijitmondal0024@gmail.com>
2021-02-28 11:08:10 +0530

    add

--- Access Modifer/test1.java
+++ Access Modifer/test1.java
@@ -1,5 +1,22 @@
-package testpackages;
+//package com.codewithme.shape;
+class Rectangle{
+    public double length;
+    public double width;
 
+    public double Aera(double l, double w){
+        double result = w*l;
+        System.out.println(result);
+        return result;
+    }
+}
+class Square{
+    public double side;
+
+    public double Side(double s){
+        System.out.println(s*s);
+        return s*s;
+    }
+}
 
 public class test1 { 
     public static void main(String args[]) {
@@ -9,6 +26,14 @@ public class test1 {
          This package should have individual classes for Rectangle, Square, Circle, Cylinder, Sphere
          These classes should use inheritance to properly manage the code!
          Include methods like volume, surface area and getters/setters for dimensions
-         */
+        */
+        Rectangle r = new Rectangle();
+        r.Aera(7,9);
+
+        Square s = new Square();
+        s.Side(9);
+
+        
+
     }
 }
