83dd37b00f95e3cf1e92be69673d0c216f5665ac
RayFisher-24 <arijitmondal0024@gmail.com>
2021-03-01 20:23:15 +0530

    add

--- test.java
+++ test.java
@@ -1,14 +1,24 @@
 class Good extends Thread{
     public void run(){
-        while(true){
+        int j =0;
+        while(j<10){
             System.out.println("Good Morning");
+            j++;
         }
     }
 }
 class Welcome extends Thread{
     public void run(){
-        while(true){
+        int i =0;
+        while(i<10){
+            try{
+                Thread.sleep(1);
+            }
+            catch (Exception e){
+                System.out.println(e);
+            }
             System.out.println("Welcome");
+            i++;
         }
     }
 }
@@ -17,8 +27,12 @@ public class test {
     public static void main(String[] args) {
        Good g = new Good();
        g.start();
+       g.setPriority(6);
+       System.out.println(g.getPriority());
        Welcome w = new Welcome();
        w.start();
+       System.out.println(w.getState());
+       System.out.println(Thread.currentThread().getState()); 
 
     }
 }
