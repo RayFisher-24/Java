ab1bd5fc37a9335f343700d058092dc93bf32a8f
RayFisher-24 <arijitmondal0024@gmail.com>
2021-03-12 11:04:38 +0530

    new practice from method

--- practice/jv3.java
+++ practice/jv3.java
@@ -21,32 +21,23 @@ public class jv3{
 
         System.out.println(sum(a,b,c,d));
     }
+    static void cGPA(){
+        int a = 45;
+        int b = 95;
+        int c = 48;
+        int cgpa = (a+b+c);
+        float result = (int)(cgpa/30);
+        System.out.println(result);
+        //System.out.println(cgpa);
+    }
 
-    public static double cGPA(int ...arr){
-        int total =0;
-        for(int e:arr){
-            total += e;
-            cgpa = total/300;
-        }
-        return cgpa;
-    }
-    public static void inputCGPA(){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the 1st Number ");
-        int a = sc.nextInt();
-        System.out.println("Enter the 2nd Number ");
-        int b = sc.nextInt();
-        System.out.println("Enter the 3rd Number ");
-        int c = sc.nextInt();
-        
-        System.out.println(cGPA(a,b,c));
-    }
     
     public static void main(String args[]) {
         System.out.println("jv3");
         
        // input();
-       inputCGPA();
+       cGPA();
+       
         
     }
 }
