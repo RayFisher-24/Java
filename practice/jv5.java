e1756e663d29b2622e64f6f5b99bed36efd5a59b
RayFisher-24 <arijitmondal0024@gmail.com>
2021-03-26 15:59:57 +0530

    ADD"
    ;

---
+++ practice/jv5.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class jv5{
+    public static int abcd(){
+        int x = 5;
+        int y = 5;
+        int c = x+y;
+        System.out.println("Hey new Life");
+        return c;
+    }
+    public static int efgh(int x){
+        return x;
+    }
+    public static float ijkl(){
+       /* int e = 6;
+        float r = 6.9f;
+        float z = e+r;*/
+        long p = 899998895656654L;
+        int q = 69;
+        int w = (int)(p+q);
+        return w;
+    }
+    public static void main(String[] args) {
+        
+        System.out.println(abcd());
+        System.out.println(efgh(5));
+        System.out.println(ijkl());
+
+
+    }
+}
