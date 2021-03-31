46c9dc4c25045398d85d4b61a56e76f29d4f83b0
RayFisher-24 <arijitmondal0024@gmail.com>
2021-03-31 21:58:16 +0530

    add

--- practice/jv6.java
+++ practice/jv6.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class jv6{
     public static int loops(){
         for(int i=1; i<10; i++){
@@ -7,6 +8,26 @@ public class jv6{
     }
     public static void main(String[] args){
         System.out.println("jv6");
-        loops();       
-    }
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        System.out.println(A.length()+B.length()); 
+        if(A.compareTo(B)<0){
+            System.out.println("No");
+            A = A.substring(0,1).toUpperCase() + A.substring(1,A.length());
+            B = B.substring(0,1).toUpperCase() + B.substring(1,B.length());
+            System.out.println(A+" "+B);
+        }else if(A.compareTo(B)==0){
+            System.out.println("No");
+            A = A.substring(0,1).toUpperCase() + A.substring(1,A.length());
+            B = B.substring(0,1).toUpperCase() + B.substring(1,B.length());
+            System.out.println(A+" "+B);
+        } else {
+            System.out.println("Yes");
+            A = A.substring(0,1).toUpperCase() + A.substring(1,A.length());
+            B = B.substring(0,1).toUpperCase() + B.substring(1,B.length());
+            System.out.println(A+" "+B);
+        } 
+        
+    }   
 }
