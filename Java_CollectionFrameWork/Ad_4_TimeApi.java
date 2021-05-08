c544dac86fc5c0fb90b8b53886558c10c87eaea9
RayFisher-24 <arijitmondal0024@gmail.com>
2021-05-09 00:14:29 +0530

    add

--- Java_CollectionFrameWork/Ad_4_TimeApi.java
+++ Java_CollectionFrameWork/Ad_4_TimeApi.java
@@ -1,5 +1,20 @@
+import java.util.*;
+import java.time.*;
+
 public class Ad_4_TimeApi{
     public static void main(String args[]) {
-        
+
+        //Calendar class refence object 
+        //Calendar c = Calendar.getInstance();
+       // System.out.println(c.getTime());
+
+        LocalTime lt = LocalTime.now();
+        System.out.println(lt);
+
+        LocalDateTime dt = LocalDateTime.now();
+        System.out.println(dt);
+
+        //Clock c = Clock.now();
+        System.out.println(System.currentTimeMillis());
     }
 }
